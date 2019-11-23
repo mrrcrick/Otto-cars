3b38ac46dc5b007b37b4836a477c55704a867f99
mrcrick <mrrcrick@gmail.com>
2019-11-23 22:43:04 +0000

    added javascript

--- clean_air_plan/assets/js/my_plugin.js
+++ clean_air_plan/assets/js/my_plugin.js
@@ -2,15 +2,41 @@
 console.log("AAAAAAAAAAAA");
 
 jQuery(function($){
+    function valid(id,discount) {
+        if (discount <2000 ||discount >5000 || isNaN(discount))
+        {
+            $("."+id+" .error-msg" ).show();
+            $("."+id+" .error-msg" ).text("** Invalid enter number from 2000 - 5000");
+            return false;
+        }
+        return true
+    }
 
+    function calculate(weeklyrent,agreelength,discount)
+    {
 
+        var totalweeks = (52 * agreelength)
+        var newtotal = (totalweeks * weeklyrent) - discount;
+        var newweekpayments = newtotal/totalweeks;
+        return "£"+parseFloat(newweekpayments).toFixed(2);
+    }
 
+    $('.rate_calculater button ').click(function(evt) {
+        var id = $(evt.target).attr('class');
+        var price = $("."+id+" .weekly_rental .info" ).text();
+        var agreelength = $("."+id+" .agreementlength .info" ).text();
+        var discount = $("."+id+" .rate_calculater input[type=text]").val();
+        $("."+id+" .error-msg" ).hide();
+        if (valid(id,discount) && discount>0) {
+            $("."+id+" .weekly_rental .discount" ).css({'color':'red','text-decoration':'line-through'});
+            $("."+id+" .weekly_rental .newprice" ).show();
+            $("."+id+" .weekly_rental .newprice" ).html(calculate(price,agreelength,discount));
+        } else {
+            $("."+id+" .weekly_rental .discount" ).css({'color':'#303e48','text-decoration':'none'});
+            $("."+id+" .weekly_rental .newprice" ).hide();
+        }
+        //color:red;text-decoration:line-through;
+        //calculate(price,agreelength,discount);
 
-        $('.rate_calculater button ').click(function(evt){
-
-                var id = $(evt.target).attr('class');
-                var price = $("."+id+" .weekly_rental" ).html();
-                alert(price);
-
-});
-});
+    });
+   });
--- clean_air_plan/clean_air_plan.php
+++ clean_air_plan/clean_air_plan.php
@@ -9,11 +9,10 @@
  */
 
  add_shortcode( 'clean_air_plan', 'clean_air_plan' );
+// load scripts
 function load_scripts() {
     wp_enqueue_style( 'my_pluggin_style', plugin_dir_url( __FILE__ ) . 'assets/css/my_pluggin_style.css',false,'1.1','all');
     wp_enqueue_style( 'bootstrap', plugin_dir_url( __FILE__ ) . 'assets/css/bootstrap.min.css',false,'1.1','all');
-    // script
-    //wp_enqueue_script( 'jquery', plugin_dir_url( __FILE__ ) . 'assets/js/jquery-3.4.1.min.js', array(), '', true);
     wp_enqueue_script( 'bootstrap', plugin_dir_url( __FILE__ ) . 'assets/js/bootstrap.min.js', array(), '', true);
     wp_enqueue_script( 'my_plugin_js', plugin_dir_url( __FILE__ ) . 'assets/js/my_plugin.js', array('jquery'), '', true);
 }
@@ -21,35 +20,46 @@ function load_scripts() {
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 $agreements = array();
 
+// set up list of cars
 function setup($agreementsfile) {
+    $listOfExpectedKeys = ['vehicle','agreement_length','weekly_rental'];
     global $agreements;
     // see if file exists
-    $string = file_get_contents(plugin_dir_url( __FILE__ )."/".$agreementsfile);
-    $agreements = json_decode($string, true);
+    if(file_exists(plugin_dir_path( __FILE__ ).$agreementsfile)){
+        $string = file_get_contents(plugin_dir_path( __FILE__ )."/".$agreementsfile);
+        $agreements = json_decode($string, true);
+    }
+    else {
+        return ("couldn't find file ".plugin_dir_path( __FILE__ ).$agreementsfile);
+    }
+    // check all field keys are present
+    foreach  ($agreements as $agreement){
+        foreach ($listOfExpectedKeys as $expectedKey) {
+            if (!array_key_exists ($expectedKey,$agreement))
+            {
+                return ($expectedKey." field is missing");
+            }
+        }
 
-    // set all field keys are present
+    }
+
+    return "Success";
 
 }
-
-
-function my_thank_you_text ( $content ) {
-    return $content .= '<p>Thank you for reading!</p>';
-}
-
-/*function first_short_code(){
-    return "Hello";
-}
-*/
+// diplay cars
 function clean_air_plan()
 {
     global $agreements;
-
-    setup('agreements.json');
-    return car_panel($agreements);
+    $result = setup('agreements.json');
+    if ( $result == "Success"){
+        return car_panel($agreements);
+    }
+    else {
+        return $result;
+    }
 }
 // panel to display car and rate_calculater
 function car_panel($agreements) {
-    //print_r($agreements);
     $panel = '';
     // get car information
     $ids = array_keys($agreements);
@@ -61,21 +71,22 @@ function car_panel($agreements) {
     return "<div style class='car_panel'>".$panel."</div>";
 
 }
-
+// card to display car information
 function car_info($id,$carinfo) {
     $HTML = '';
     $baseUrl = plugin_dir_url( __FILE__ )."assets/images/";
     $HTML = "<div class='".$id." carcard'>".
-    "<div class='carimage' style ='background-image: url(".'"'.$baseUrl.$id.'.png"'.");background-size: 100% 100%;'> </div>".
-    "<div class='vechicle'>".$carinfo['vehicle']."</div>".
-    "<div class='agreementlength'>agreement length: ".$carinfo['agreement_length']."</div>".
-    "<div class='weekly_rental'>weekly rental: ".$carinfo['weekly_rental']."</div>".
+    "<div class='carimage' style ='background-image: url(".'"'.$baseUrl.$id.'.png"'.");background-size: 100% 100%;'><span class='error-msg'></span></div>".
+    "<div class='vehicle'>".$carinfo['vehicle']."</div>".
+    "<div class='agreementlength'>agreement length: <span class='info'>".$carinfo['agreement_length']."</span> years</div>".
+    "<div class='weekly_rental'>weekly rental: <span class='discount'>£<span class='info'>".$carinfo['weekly_rental'].
+    "</span></span><span class='newprice'>0</span></div>".
     rate_calculater($id)."</div>";
-    // add the rate_calculater
     return $HTML;
 }
+// rate calculator
 function rate_calculater($id) {
-    $html = "<div class='rate_calculater'><b>Enter Green Miles : </b><input type='text'>
+    $html = "<div class='rate_calculater'><b>Uber Clean Air fund : £</b><input type='text'>
     <button type='button' class='".$id."'><b class='".$id."'>Calculate Discount</b></button>
     </div>";
     return $html;
